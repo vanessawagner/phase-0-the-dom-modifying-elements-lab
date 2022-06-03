bfe83566210cb9be824e6a68bef728d54a85e915
Vanessa Wagner <vmwagner@gmail.com>
2022-06-03 09:21:36 -0500

    commit tree 913880982df113072da915520a82e2d7772b19ba

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Write your code here!
+const body = document.querySelector("body")
+const main = body.querySelector("main");
+body.removeChild(main)
+
+const newHeader = document.createElement("h1")
+newHeader.id = 'victory'
+newHeader.textContent = "YOUR NAME is the champion";
+
+
+document.body.append(newHeader);
+
